acf4afa5cfb43c17df74eb0625491182da3bd721
MuarEnok <arumkone@wrdf.org>
2014-03-13 14:28:08 -0700

    Added QCEW data & maps

--- US county Map Oil and Gas production by year.R
+++ US county Map Oil and Gas production by year.R
@@ -1,17 +1,19 @@
 # map of oil and gas by year
 library(choroplethr)
 
-gas <- read.csv("/Users/ak/Dev/r/choroplethr/data/oil/oilgascounty.csv",  header=TRUE, as.is=TRUE)
-title = paste(gas[1, ])
-title = paste(gas[1, "gas2011"])
-gas = gas[-1, c("FIPS", "oil2011")]
+gas <- read.csv("data/oil/oilgascounty.csv",  header=TRUE, as.is=TRUE)
+title = paste(gas[1, 9:35 ])
+title.map = paste(gas[1, "oil2011"])
+title.map = gsub('for the year specified in the variable name', "2011", title.map)
+gas = gas[-1, c("FIPS", "oil2011", "Stabr" )]
 names(gas) = c("region", "value")
+
 gas$region <- as.numeric(gas$region)
-
-
+# remove the comma from the text string so it can be made into proper numeric
 gas$value <- as.numeric(gsub(",", "", gas$value))
-gas$r <- as.numeric(cut(gas$region, breaks=8, include.lowest=TRUE))
 
-choroplethr(gas, lod="county", num_buckets=8)
+choroplethr(gas, lod="county", num_buckets=8, title = title.map, states= western_states)
 
-str(gas)
+# gas$r <- as.numeric(cut(gas$region, breaks=8, include.lowest=TRUE))
+# gas <- subset(gas,  (gas$Stabr %in% western_states))
+# western_states = c("WA", "OR", "ID", "MT", "WY", "SD", "ND")
