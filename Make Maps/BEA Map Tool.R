c2da8c0b0f32534092dd9dc86953351e9bdcda42
MuarEnok <arumkone@wrdf.org>
2014-03-14 13:19:35 -0700

    Changed flow to one map with manual option for side by side maps see notes

---
+++ Make Maps/BEA Map Tool.R
@@ -0,0 +1,66 @@
+library(choroplethr)
+library(gridExtra)
+library(ggplot2)
+
+
+# get bear facts files into a datatable for all counties
+# https://www.bea.gov/regional/downloadzip.cfm
+loc = dir("/Users/ak/Data/BEA/CA91/", pattern=".csv")
+# remove strange files Use for CA04 not other folders
+loc = loc[- c(1, 7,  13, 23,  26, 30, 44, 50)]
+
+base <- "/Users/ak/Data/BEA/CA91/" 
+files <- lapply(loc, function(.state){ 
+  cat(.state, "\n") 
+  input <- read.csv(paste0(base, .state)
+                      , sep=","
+                      , header=FALSE
+                      , as.is=TRUE
+  )
+  
+  
+  input
+})
+# this creates the data table for the set
+CA04 <- do.call(rbind, files)
+rm(x)
+rm(files)
+
+
+x <- names(read.csv(paste0(base, loc[3]), sep=",", header=TRUE, as.is=TRUE))
+names(CA04) <- x
+df <- CA04[-1,]
+
+
+trim.leading <- function (x)  sub("^\\s+", "", x)
+df$LineTitle <- trim.leading(df$LineTitle)
+
+# get categories to map 
+cat.var = paste(unique(df$LineTitle))
+cat.var
+# get year
+cat.year = paste(names(df)[8:length(names(df))])
+cat.view = paste(seq(8:length(names(df))),names(df)[8:length(names(df))], sep=" : ")
+cat.view
+
+cat = 1            # Put the index for category titles here see console for choices
+# Place index number here for year
+yr = 23             # put the year from cat.year view in index <-
+
+
+# use the index to subset the mapping category and grab title
+line = paste(cat.var[cat])
+title.var = paste(line)
+# subset farm income
+cat.year = paste(cat.year[yr])
+df <- subset(df, LineTitle == line)[, c("FIPS", cat.year )]
+
+df$region <- as.numeric(df$FIPS)
+df$value <- as.numeric(df[,2])
+
+# choroplethr(df, "county", title= title.var)
+grid.arrange(inflow, outflow, nrow=1, ncol=2)
+outflow <- choroplethr(df, "county", title= title.var)
+inflow <- choroplethr(df, "county", title= title.var)
+
+
